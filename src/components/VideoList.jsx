8bd608d494f92bd981cfd3d743a6b2900a0b8368
AbdoAlabed <henry_3030@icloud.com>
2019-12-09 20:01:20 +0100

    second

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,10 @@
-var VideoList = () => (
+import VideoListEntry from "VideoListEntry"
+var VideoList = (videos) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <VideoListEntry video={videos[0]}/>
+    <VideoListEntry video={videos[1]}/>
+    <VideoListEntry video={videos[2]}/>
+    <VideoListEntry video={videos[3]}/>
   </div>
 );
 
